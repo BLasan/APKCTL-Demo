87e42e6facd3baee766f2b888be28d18315dd2f3
BLasan <benuraab@gmail.com>
2022-10-06 21:53:47 +0530

    Remove printing command

--- k8s/utils.go
+++ k8s/utils.go
@@ -20,7 +20,6 @@ package k8s
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"os"
 	"os/exec"
@@ -29,7 +28,7 @@ import (
 // ExecuteCommand executes the command with args and prints output, errors in standard output, error
 func ExecuteCommand(command string, args ...string) error {
 	cmd := exec.Command(command, args...)
-	fmt.Println("CMD: ", cmd)
+	// fmt.Println("CMD: ", cmd)
 	setCommandOutAndError(cmd)
 	return cmd.Run()
 }
